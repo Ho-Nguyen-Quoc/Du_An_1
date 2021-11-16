4fbcc92e13b723d45f5e7759bee415dcfe02a656
hoangit2k2 <nguyenhoang2002it@gmail.com>
2021-11-16 10:40:24 +0700

    update

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgithub;
+
+/**
+ *
+ * @author thinh
+ */
+public class TestGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Code by Thinh");
+    }
+  //  h ông thấy code của tui rồi
+//            Cái nầy mình code xong nó có lưu lên github ko ông
+    //có ông 
+    //mỗi lần code xong ông vào githubdesktop update lên
+}
